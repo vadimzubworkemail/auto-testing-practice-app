f088ce50bc7643e031dfdf62e4e9d11e68460c22
Vadim Zub <vvzub@vtb.ru>
2023-05-30 09:52:11 +0300

    first commit

---
+++ src/main/java/com/example/autotestingpracticeapp/AutoTestingPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.autotestingpracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestingPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestingPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autotestingpracticeapp/AutoTestingPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autotestingpracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestingPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
