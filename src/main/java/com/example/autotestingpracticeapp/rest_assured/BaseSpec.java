d10283cb74477d8cce0b5314c2a17dc323762670
Anatoliy Samoylov <a.samoylov@vtb.ru>
2023-06-07 12:49:50 +0300

    edited BaseSpec

--- src/main/java/com/example/autotestingpracticeapp/rest_assured/BaseSpec.java
+++ src/main/java/com/example/autotestingpracticeapp/rest_assured/BaseSpec.java
@@ -5,8 +5,8 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class BaseSpec {
-    public RequestSpecification getBaseSpec (){
-        String URL = "http://localhost:8080/";
+    public RequestSpecification getBaseSpec(){
+        String URL = "http://localhost:8080";
         return new RequestSpecBuilder()
                 .setBaseUri(URL)
                 .setContentType(ContentType.JSON)
